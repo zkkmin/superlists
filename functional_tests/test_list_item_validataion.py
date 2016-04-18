caa300c592c203ee8f27e0a62518cbdf11351373
Zin Ko Ko Min <zkkmin@gmail.com>
2016-04-18 10:30:58 +0800

    Split out unit tests into two files

--- functional_tests/test_list_item_validataion.py
+++ functional_tests/test_list_item_validataion.py
@@ -30,6 +30,5 @@ class ItemValidataionTest(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('1: Make tea')
-        self.fail('write me!')
         
 
