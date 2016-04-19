27013a95d8ea55e6f839c3c8a0cbd7e4cbb186db
Zin Ko Ko Min <zkkmin@gmail.com>
2016-04-19 15:07:56 +0800

    Final commit for Chapter 11

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,49 @@
+from unittest import skip
+from .base import FunctionalTest
+    
+class ItemValidationTest(FunctionalTest):
+
+    def test_cannot_add_empty_list_items(self):
+        # Edit goes to the home page and accidendtally tries to submit
+        # an empty list item, She hits Enter on the empty input box
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        # She tries again with some text for the item, which now works
+        self.get_item_input_box().send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        # Perversely, she now decides to submit a second balnk list item
+        self.get_item_input_box().send_keys('\n')
+       
+        # She receives a similar warning on the list page
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+        
+        # And she can correct it by fillign some text in
+        self.get_item_input_box().send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
+        
+
+    def test_cannot_add_duplicate_items(self):
+        # Edith goes to the home page and starts a new list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy wellies\n')
+        self.check_for_row_in_list_table('1: Buy wellies')
+        
+        # she accidently tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Buy wellies\n')
+
+        # she sees a helpful error message
+        self.check_for_row_in_list_table('1: Buy wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
+
+
